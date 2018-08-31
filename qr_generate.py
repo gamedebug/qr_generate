8dcfb7792f6c89c88dda7edb2f5a41adb67b5cd3
陆翊司 <gamedebug@vip.qq.com>
2018-09-01 03:01:15 +0800

    qrcode

--- qr_generate.py
+++ qr_generate.py
@@ -1,10 +1,9 @@
 #!/anaconda2/bin/python
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
 
 from MyQR import myqr
 
-words = raw_input("请输入二维码内容: ");
+words = raw_input("Enter your text: ");
 
 myqr.run(
     words,
